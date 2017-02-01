5d0405de81139346db786f18ef1f31e08fd7a62e
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-01 15:14:35 +0700

    done

--- method_chaining.js
+++ method_chaining.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const shout_backwards = (data) => {
   let allcaps = data.toUpperCase()
   let splitting = allcaps.split('')
@@ -6,17 +8,22 @@ const shout_backwards = (data) => {
   return backwards + '!!!'
 }
 
+const methodChaining = (word) => {
+  return word.toUpperCase().split('').reverse().join('')
+}
+
 console.log(shout_backwards('Hello World'))
+console.log(methodChaining('Hello World'))
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const example = word
-  .allcaps()
-  .splitting()
-  .reversing()
-  .backwards()
-  .addsuffix()
-  .print()
+// const example = shout_backwards(word)
+//   .allcaps()
+//   .splitting()
+//   .reversing()
+//   .backwards()
+//   .addsuffix()
+//   .print()
 
 // Alternatif penulisan:
 // word.allcaps().splitting().reversing().backwards().addsuffix().print()
