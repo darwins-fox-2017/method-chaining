f40fa1286cd4256b4609ce456321d1fc5cc5aae9
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-01 17:07:38 +0700

    Done all release

--- method_chaining.js
+++ method_chaining.js
@@ -1,3 +1,4 @@
+
 const shout_backwards = (data) => {
   let allcaps = data.toUpperCase()
   let splitting = allcaps.split('')
@@ -10,6 +11,43 @@ console.log(shout_backwards('Hello World'))
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
+
+const convert = function(string){
+
+        this.allcaps = function(){
+          string = string.toUpperCase()
+          return this
+        }
+
+        this.splitting = function(){
+          string = string.split('')
+          return this
+        }
+
+        this.reversing = function(){
+          string = string.reverse()
+          return this
+        }
+
+        this.backwards = function(){
+          string = string.join('')
+          return this
+        }
+
+        this.addsuffix = function(){
+          string += '!!!'
+          return this
+        }
+
+        this.print = function(){
+          console.log(string)
+          return this
+        }
+
+      }
+
+const word = new convert('Hello World')
+
 const example = word
   .allcaps()
   .splitting()
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -22,16 +22,51 @@ const titleCaseName = (string) => {
 
 // Our object with the chainable methods using class in ES6
 class Program {
+  constructor(source){
+    this.data = source
+    this.user = null
+  }
+
   titleCaseName (string) {
     // ...
+  }
+
+
+  findUser(emailUser){
+    for(let i=0; i<this.data.length; i++){
+      if(this.data[i].email == emailUser){
+        this.user = this.data[i]
+        break;
+      }
+    }
+    return this
+  }
+
+  formatName(){
+    this.user.fullName = `${this.user.firstName} ${this.user.lastName}`
+    return this
+  }
+
+  formatData(){
+    this.user.joinData = `Member name: ${this.user.fullName} \nID: ${this.user.id} \nEmail: ${this.user.email}`
+    return this
+  }
+
+  displayUser(){
+    console.log(this.user.joinData)
+    return this
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const program = new Program()
+const program = new Program(data)
 program
   .findUser('spongebob@crustycrab.com')
+  .formatName()
+  .formatData()
+  .displayUser()
+  .findUser('awesomesquidward@yahoo.com')
   .formatName()
   .formatData()
   .displayUser()
