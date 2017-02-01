f8a706a6947c53a97662de80ec30d64ff891be7d
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-01 19:50:15 +0700

    revisi

--- method_chaining.js
+++ method_chaining.js
@@ -8,22 +8,59 @@ const shout_backwards = (data) => {
   return backwards + '!!!'
 }
 
-const methodChaining = (word) => {
-  return word.toUpperCase().split('').reverse().join('')
-}
+// const methodChaining = (word) => {
+//   return word.toUpperCase().split('').reverse().join('')
+// }
 
 console.log(shout_backwards('Hello World'))
-console.log(methodChaining('Hello World'))
+// console.log(methodChaining('Hello World'))
+
+class Kata {
+  constructor (word) {
+    this.word = word;
+  }
+
+  allcaps() {
+    this.word = this.word.toUpperCase()
+    return this
+  }
+
+  splitting() {
+    this.word = this.word.split('')
+    return this
+  }
+
+  reversing() {
+    this.word = this.word.reverse()
+    return this
+  }
+
+  backwards() {
+    this.word = this.word.join('')
+    return this
+  }
+
+  addsuffix() {
+    this.word = this.word + ' !!!!'
+    return this
+  }
+
+  print() {
+    console.log(this.word);
+  }
+}
+
+var word = new Kata('Hello World')
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-// const example = shout_backwards(word)
-//   .allcaps()
-//   .splitting()
-//   .reversing()
-//   .backwards()
-//   .addsuffix()
-//   .print()
+const example = word
+  .allcaps()
+  .splitting()
+  .reversing()
+  .backwards()
+  .addsuffix()
+  .print()
 
 // Alternatif penulisan:
 // word.allcaps().splitting().reversing().backwards().addsuffix().print()
