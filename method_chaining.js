2259b2d2deec073d7450bbefc754c075a0096097
Diky Arga <dikyarga.id@gmail.com>
2017-02-02 21:51:59 +0700

    release 0 - simplify with chaining

--- method_chaining.js
+++ method_chaining.js
@@ -6,17 +6,55 @@ const shout_backwards = (data) => {
   return backwards + '!!!'
 }
 
-console.log(shout_backwards('Hello World'))
+// console.log(shout_backwards('Hello World'))
 
+class stringManipulation {
+  constructor(string) {
+    this.string = string
+  }
+
+  allcaps(){
+    this.string = this.string.toUpperCase()
+    return this
+  }
+
+  splitting(){
+    this.string = this.string.split('')
+    return this
+  }
+
+  reversing(){
+    this.string = this.string.reverse()
+    return this
+  }
+
+  joining(){
+    this.string = this.string.join('')
+    return this
+  }
+
+  addsuffix(){
+    this.string += '!!!'
+    return this
+  }
+
+  display(){
+    return this.string
+  }
+}
+let string = 'hallo dunia'
+
+let stringManipulation1 = new stringManipulation(string)
+console.log(stringManipulation1.allcaps().splitting().reversing().joining().addsuffix().display());
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const example = word
-  .allcaps()
-  .splitting()
-  .reversing()
-  .backwards()
-  .addsuffix()
-  .print()
+// const example = word
+//   .allcaps()
+//   .splitting()
+//   .reversing()
+//   .backwards()
+//   .addsuffix()
+//   .print()
 
 // Alternatif penulisan:
 // word.allcaps().splitting().reversing().backwards().addsuffix().print()
