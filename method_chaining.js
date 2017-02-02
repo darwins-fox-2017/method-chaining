5cffac855df423a923c6a98c420b53e31af0942f
isumizumi <isumi.karina72@gmail.com>
2017-02-02 08:15:30 +0700

    Finish

--- method_chaining.js
+++ method_chaining.js
@@ -1,22 +1,59 @@
 const shout_backwards = (data) => {
-  let allcaps = data.toUpperCase()
-  let splitting = allcaps.split('')
-  let reversing = splitting.reverse()
-  let backwards = reversing.join('')
-  return backwards + '!!!'
+  // let allcaps = data.toUpperCase()
+  // let splitting = allcaps.split('')
+  // let reversing = splitting.reverse()
+  // let backwards = reversing.join('')
+  return data.toUpperCase().split('').reverse().join('') + '!!!'
 }
 
 console.log(shout_backwards('Hello World'))
 
 // -----------------------------------------------------------------------------
+
+class Sentence {
+  constructor(word){
+    this.word = word
+  }
+
+  allcaps() {
+    this.word = this.word.toUpperCase()
+    return word
+  }
+
+  splitting() {
+    this.word = this.word.split('')
+    return word
+  }
+
+  reversing() {
+    this.word = this.word.reverse()
+    return word
+  }
+
+  backwards() {
+    this.word = this.word.join('')
+    return word
+  }
+
+  addsuffix() {
+    this.word = this.word + '!!!'
+    return word
+  }
+
+  print() {
+    console.log(this.word)
+  }
+}
+
+var word = new Sentence("Hello World")
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const example = word
-  .allcaps()
-  .splitting()
-  .reversing()
-  .backwards()
-  .addsuffix()
-  .print()
+// const example = word
+//   .allcaps()
+//   .splitting()
+//   .reversing()
+//   .backwards()
+//   .addsuffix()
+//   .print()
 
 // Alternatif penulisan:
-// word.allcaps().splitting().reversing().backwards().addsuffix().print()
+word.allcaps().splitting().reversing().backwards().addsuffix().print()
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -16,20 +16,46 @@ let data = [{
   id: 103
 }]
 
-const titleCaseName = (string) => {
-  // ...
-}
+// const titleCaseName = (string) => {}
 
 // Our object with the chainable methods using class in ES6
 class Program {
-  titleCaseName (string) {
-    // ...
+  constructor (value) {
+    this.data = value
+    this.user = ""
+  }
+
+  // titleCaseName (string) {
+  //   // ...
+  // }
+
+  findUser (emailUser) {
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].email == emailUser) {
+        this.user = this.data[i]
+      }
+    }
+    return this
+  }
+
+  formatName () {
+    this.user.userName = `${this.user.firstName} ${this.user.lastName}`
+    return this
+  }
+
+  formatData () {
+    this.user.format = `Member name: ${this.user.userName}\nID: ${this.user.id}\nEmail: ${this.user.email}`
+    return this
+  }
+
+  displayUser () {
+    console.log(this.user.format)
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const program = new Program()
+const program = new Program(data)
 program
   .findUser('spongebob@crustycrab.com')
   .formatName()
