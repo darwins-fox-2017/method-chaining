ad912ef16df67a3815d3e7f86b3347c6629e715e
Eri Irawan <eryirawan91@gmail.com>
2017-02-02 19:45:56 +0700

    done all

--- method_chaining.js
+++ method_chaining.js
@@ -1,12 +1,12 @@
-// const shout_backwards = (data) => {
-//   let allcaps = data.toUpperCase()
-//   let splitting = allcaps.split('')
-//   let reversing = splitting.reverse()
-//   let backwards = reversing.join('')
-//   return backwards + '!!!'
-// }
-//
-// console.log(shout_backwards('Hello World'))
+const shout_backwards = (data) => {
+  let allcaps = data.toUpperCase()
+  let splitting = allcaps.split('')
+  let reversing = splitting.reverse()
+  let backwards = reversing.join('')
+  return backwards + '!!!'
+}
+
+console.log(shout_backwards('Hello World'))
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
