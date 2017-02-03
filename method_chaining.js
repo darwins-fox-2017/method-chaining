fa58067243b7b0458b57111a0675f3083f025484
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-03 20:13:03 +0700

    Danang method chaining

--- method_chaining.js
+++ method_chaining.js
@@ -1,13 +1,46 @@
 const shout_backwards = (data) => {
-  let allcaps = data.toUpperCase()
-  let splitting = allcaps.split('')
-  let reversing = splitting.reverse()
-  let backwards = reversing.join('')
+  let backwards = data.toUpperCase().split('').reverse().join('')
   return backwards + '!!!'
 }
 
 console.log(shout_backwards('Hello World'))
 
+class Word {
+  constructor(word) {
+    this.word = word
+  }
+
+  allcaps() {
+    this.word = this.word.toUpperCase()
+    return this
+  }
+
+  splitting(delimiter)
+    this.word = this.word.split(delimiter)
+    return this
+  }
+
+  reversing() {
+    this.word = this.word.reverse()
+    return this
+  }
+
+  backwards(delimiter) {
+    this.word = this.word.join(delimiter)
+    return this
+  }
+
+  addsuffix() {
+    this.word = this.word + '!!!'
+    return this
+  }
+
+  print() {
+    console.log(this.word)
+  }
+}
+
+const word = new Word('Danang Aji Tamtomo');
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
 const example = word
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -1,29 +1,68 @@
 // The data store:
 let data = [{
   firstName: 'SpongeBob',
-  lastName: 'SquarePants',
-  email: 'spongebob@crustycrab.com',
-  id: 101
+  lastName : 'SquarePants',
+  email    : 'spongebob@crustycrab.com',
+  id       : 101
 }, {
   firstName: 'Patrick',
-  lastName: 'Star',
-  email: 'patric.star@gmail.com',
-  id: 102
+  lastName : 'Star',
+  email    : 'patric.star@gmail.com',
+  id       : 102
 }, {
   firstName: 'Squidward',
-  lastName: 'Tentacles',
-  email: 'awesomesquidward@yahoo.com',
-  id: 103
+  lastName : 'Tentacles',
+  email    : 'awesomesquidward@yahoo.com',
+  id       : 103
 }]
+
+console.log();
 
 const titleCaseName = (string) => {
   // ...
+
 }
 
 // Our object with the chainable methods using class in ES6
 class Program {
+  constructor() {
+    this.storage = data
+    this.user
+    this.concatedName
+    this.formatedUser
+  }
+
+  findUser(email) {
+    for (let i = 0; i < this.storage.length; i++) {
+      if(this.storage[i].email === email) {
+        this.user = this.storage[i]
+      }
+    }
+    return this
+  }
+
+  formatName() {
+    this.concatedName = this.user.firstName+' '+this.user.lastName  
+    return this
+  }
+
+  formatData() {
+    this.formatedUser = `
+      Hasil:
+      Member name : ${this.concatedName}
+      ID : ${this.user.id}
+      email : ${this.user.email}
+    ` 
+    return this
+  }
+
+  displayUser() {
+    console.log(this.formatedUser)
+  }
+
   titleCaseName (string) {
     // ...
+    
   }
 }
 
