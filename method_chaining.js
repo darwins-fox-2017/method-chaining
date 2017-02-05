90932f60304f40be0521900a5bf71a87b64a0ea9
pisanggoreng <7ofpentacles@gmail.com>
2017-02-05 23:20:36 +0700

    clear

--- method_chaining.js
+++ method_chaining.js
@@ -4,6 +4,40 @@ const shout_backwards = (data) => {
 
 console.log(shout_backwards('Hello World'))
 
+class Chaining {
+  constructor (string) {
+    this.string = string
+  }
+
+  allcaps() {
+    this.string = this.string.toUpperCase()
+    return this
+  }
+
+  splitting() {
+    this.string = this.string.split('')
+    return this
+  }
+
+  reversing() {
+    this.string = this.string.reverse()
+    return this
+  }
+
+  backwards() {
+    this.string = this.string.join('')
+    return this
+  }
+
+  addsuffix() {
+    this.string += "!!!!!!!!!!!"
+    return this
+  }
+
+  print() {
+    console.log(this.string);
+  }
+}
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
 // const example = word
@@ -15,4 +49,5 @@ console.log(shout_backwards('Hello World'))
 //   .print()
 
 // Alternatif penulisan:
-// word.allcaps().splitting().reversing().backwards().addsuffix().print()
+let word = new Chaining('Hello world')
+word.allcaps().splitting().reversing().backwards().addsuffix().print()
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -22,16 +22,52 @@ const titleCaseName = (string) => {
 
 // Our object with the chainable methods using class in ES6
 class Program {
-  titleCaseName (string) {
-    // ...
+  // titleCaseName (string) {
+  //   // ...
+  // }
+
+  constructor() {
+    this.usersData = data;
+    this.userIndex = 0
+    this.nama = ''
+    this.dataNama = ''
+  }
+
+  findUser(email) { // output = lokasi index ke 2
+    for ( var i = 0; i < this.usersData.length; i++ ) {
+      if ( this.usersData[i].email == email ) {
+        this.userIndex = i;
+        //console.log(this.usersData[i]);
+        return this;
+      }
+    }
+  }
+
+  formatName() {
+    var index = this.userIndex;
+    this.nama = this.usersData[index].firstName + " " + this.usersData[index].lastName;
+    //console.log(this.nama);
+    return this
+  }
+
+  formatData() {
+    var index = this.userIndex;
+    this.dataNama = `Member name: ${this.nama} \nID: ${this.usersData[index].id} \nEmail: ${this.usersData[index].email}`
+    return this
+  }
+
+  displayUser() {
+    console.log('');
+    console.log(this.dataNama);
+    return this.dataNama
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
+// oke... siappppp
 const program = new Program()
-program
-  .findUser('spongebob@crustycrab.com')
+program.findUser('spongebob@crustycrab.com')
   .formatName()
   .formatData()
   .displayUser()
