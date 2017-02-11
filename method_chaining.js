0b7a3bfb6db9419f220b07607b1c2a07ff0837bd
adamsaparudin <adamsaparudin@gmail.com>
2017-02-11 14:03:49 +0700

    SELESAI

--- method_chaining.js
+++ method_chaining.js
@@ -1,22 +1,50 @@
-const shout_backwards = (data) => {
-  let allcaps = data.toUpperCase()
-  let splitting = allcaps.split('')
-  let reversing = splitting.reverse()
-  let backwards = reversing.join('')
-  return backwards + '!!!'
+'use strict'
+
+// const shout_backwards = (data) => {
+//   let allcaps = data.toUpperCase()
+//   let splitting = allcaps.split('')
+//   let reversing = splitting.reverse()
+//   let backwards = reversing.join('')
+//   return backwards + '!!!'
+// }
+
+class shout_backwards {
+  constructor(word) {
+    this.words = word;
+  }
+  allcaps() {
+    this.words = this.words.toUpperCase();
+    return this
+  }
+  splitting(x) {
+    this.words = this.words.split(x)
+    return this
+  }
+  reversing() {
+    this.words = this.words.reverse()
+    return this
+  }
+  backwards(x) {
+    this.words = this.words.join(x)
+    return this
+  }
+  printerr() {
+    console.log(this.words, "!!!")
+  }
 }
 
-console.log(shout_backwards('Hello World'))
+let shoutBack = new shout_backwards('Hello World')
+shoutBack.allcaps().printerr()
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const example = word
-  .allcaps()
-  .splitting()
-  .reversing()
-  .backwards()
-  .addsuffix()
-  .print()
+// const example = word
+//   .allcaps()
+//   .splitting()
+//   .reversing()
+//   .backwards()
+//   .addsuffix()
+//   .print()
 
 // Alternatif penulisan:
 // word.allcaps().splitting().reversing().backwards().addsuffix().print()
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -25,6 +25,34 @@ class Program {
   titleCaseName (string) {
     // ...
   }
+  constructor(){
+    this.input = data
+    this.temp  = ""
+    this.result= []
+  }
+
+  findUser(string){
+    this.string = string
+    return this
+  }
+
+  formatName(){
+    for (let i = 0; i < this.input.length; i++) {
+      if(this.string == this.input[i].email){
+        this.temp = i
+        return this
+      }
+    }
+  }
+
+  formatData(){
+    this.result.push(this.input[this.temp])
+    return this
+  }
+
+  displayUser(){
+    console.log(`Member Name : ${this.result[0]['firstName']} ${this.result[0]['lastName']}\nID : ${this.result[0]['id']}\nEmail : ${this.result[0]['email']}`)
+  }
 }
 
 // -----------------------------------------------------------------------------
